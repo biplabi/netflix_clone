5cec39f7f505e00a50060fe198bf41c3d4647dd2
biplabi <biplabi555@gmail.com>
2024-03-10 20:59:58 +0530

    push

---
+++ netflix-api/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/UserRoutes");
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/netflix", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("DB Connected");
+})
+.catch((err) => {
+    console.error("Error connecting to the database:", err);
+});
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, console.log("server started on port: 5000"));
