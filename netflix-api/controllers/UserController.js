ab89eb2a014951e918a5f3f5cf4d4181a13b65d6
biplabi <biplabi555@gmail.com>
2024-03-20 22:45:53 +0530

    push

--- netflix-api/controllers/UserController.js
+++ netflix-api/controllers/UserController.js
@@ -7,7 +7,7 @@ module.exports.addToLikedMovies = async (req, res) => {
         const user = await User.findOne({email});
         if(user) {
             const {likedMovies} = user;
-            const movieAlreadyLiked = likedMovies.find(({id}) => (id = data.id));
+            const movieAlreadyLiked = likedMovies.find(({id}) => (id === data.id));
             if(!movieAlreadyLiked){
                 await User.findByIdAndUpdate(
                     user._id,
@@ -23,3 +23,41 @@ module.exports.addToLikedMovies = async (req, res) => {
         return res.json({msg: "Error adding movies"});
     }
 };
+
+module.exports.getLikedMovies = async (req, res) => {
+    try {
+      const { email } = req.params;
+      const user = await User.findOne({ email });
+      if (user) {
+        return res.json({ msg: "success", movies: user.likedMovies });
+      } else return res.json({ msg: "User with given email not found." });
+    } catch (error) {
+      return res.json({ msg: "Error fetching movies." });
+    }
+  };
+
+
+  module.exports.removeFromLikedMovies = async (req, res) => {
+    try {
+      const { email, movieId } = req.body;
+      const user = await User.findOne({ email });
+      if (user) {
+        const movies = user.likedMovies;
+        const movieIndex = movies.findIndex(({ id }) => id === movieId);
+        if (!movieIndex) {
+          res.status(400).send({ msg: "Movie not found." });
+        }
+        movies.splice(movieIndex, 1);
+        await User.findByIdAndUpdate(
+          user._id,
+          {
+            movies,
+          },
+          { new: true }
+        );
+        return res.json({ msg: "Movie successfully removed.", movies });
+      } else return res.json({ msg: "User with given email not found." });
+    } catch (error) {
+      return res.json({ msg: "Error removing movie to the liked list" });
+    }
+  };
